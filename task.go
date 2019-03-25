14b3e31ef8e624ee17f846ec813bd35c9a5430c2
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2019-03-25 23:52:34 +0900

    Create TaskManager

--- task.go
+++ task.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/google/uuid"
 	"github.com/guregu/dynamo"
 	"github.com/pkg/errors"
@@ -22,16 +24,39 @@ type Task struct {
 	deleted     bool
 }
 
+type TaskManager struct {
+	ssn   *session.Session
+	cfg   *aws.Config
+	table dynamo.Table
+}
+
+func NewTaskManager(region, tableName string) TaskManager {
+	cfg := &aws.Config{Region: aws.String(region)}
+	ssn := session.Must(session.NewSession(cfg))
+	db := dynamo.New(session.New(), cfg)
+
+	taskMgr := TaskManager{
+		cfg:   cfg,
+		ssn:   ssn,
+		table: db.Table(tableName),
+	}
+
+	return taskMgr
+}
+
 func toPKey(userID string, date time.Time, taskID string) string {
 	return fmt.Sprintf("%s/task/%s/%s", userID, date.Format("20060102"), taskID)
 }
+func toPKeyPrefix(userID string, date time.Time) string {
+	return fmt.Sprintf("%s/task/%s/", userID, date.Format("20060102"))
+}
 
-func NewTask(table dynamo.Table, userID string, date time.Time) *Task {
+func (x TaskManager) NewTask(userID string, date time.Time) *Task {
 	task := Task{
 		UserID:    userID,
 		TaskID:    strings.Replace(uuid.New().String(), "-", "", -1),
 		CreatedAt: date,
-		table:     table,
+		table:     x.table,
 	}
 
 	task.PKey = toPKey(task.UserID, task.CreatedAt, task.TaskID)
@@ -39,9 +64,9 @@ func NewTask(table dynamo.Table, userID string, date time.Time) *Task {
 	return &task
 }
 
-func GetTask(table dynamo.Table, userID string, date time.Time, taskID string) (*Task, error) {
+func (x TaskManager) GetTask(userID string, date time.Time, taskID string) (*Task, error) {
 	var task Task
-	if err := table.Get("pk", toPKey(userID, date, taskID)).One(&task); err != nil {
+	if err := x.table.Get("pk", toPKey(userID, date, taskID)).One(&task); err != nil {
 		if err.Error() == "dynamo: no item found" {
 			return nil, nil
 		}
@@ -50,6 +75,18 @@ func GetTask(table dynamo.Table, userID string, date time.Time, taskID string) (
 	}
 
 	return &task, nil
+}
+
+func (x TaskManager) FetchTasks(userID string, date time.Time) ([]Task, error) {
+	var tasks []Task
+
+	query := x.table.Scan().Filter("begins_with($, ?)", "pk", toPKeyPrefix(userID, date))
+
+	if err := query.All(&tasks); err != nil {
+		return nil, errors.Wrapf(err, "Fail to fetch tasks: %s %s", userID, date)
+	}
+
+	return tasks, nil
 }
 
 func (x *Task) Save() error {
@@ -68,14 +105,3 @@ func (x *Task) Delete() error {
 	x.deleted = true
 	return nil
 }
-
-/*
-func FetchTasks(table dynamo.Table, userID string, date time.Time) ([]Task, error) {
-	var tasks []Task
-	query := toPKey(, ).Filter("begins_with($, ?)", "rk", date.Format("20060102/")
-	if err := table.Get("pk", ).All(&tasks); err != nil {
-		return nil, errors.Wrapf(err, "Fail to fetch tasks: %s %s", userID, date)
-	}
-	return tasks, nil
-}
-*/
--- task_test.go
+++ task_test.go
@@ -4,10 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/google/uuid"
-	"github.com/guregu/dynamo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -15,17 +12,15 @@ import (
 )
 
 func TestNewTask(t *testing.T) {
+	mgr := main.NewTaskManager(testCfg.TableRegion, testCfg.TableName)
 	uid1 := uuid.New().String()
 	now := time.Now()
 
-	db := dynamo.New(session.New(), &aws.Config{Region: aws.String(testCfg.TableRegion)})
-	table := db.Table(testCfg.TableName)
-
-	t1 := main.NewTask(table, uid1, now)
+	t1 := mgr.NewTask(uid1, now)
 	err := t1.Save()
 	require.NoError(t, err)
 
-	t2, err := main.GetTask(table, uid1, now, t1.TaskID)
+	t2, err := mgr.GetTask(uid1, now, t1.TaskID)
 	require.NoError(t, err)
 	require.NotNil(t, t2)
 	assert.Equal(t, uid1, t2.UserID)
@@ -33,7 +28,27 @@ func TestNewTask(t *testing.T) {
 	err = t1.Delete()
 	require.NoError(t, err)
 
-	t3, err := main.GetTask(table, uid1, now, t1.TaskID)
+	t3, err := mgr.GetTask(uid1, now, t1.TaskID)
 	require.NoError(t, err)
 	require.Nil(t, t3)
+}
+
+func TestFetchTasks(t *testing.T) {
+	mgr := main.NewTaskManager(testCfg.TableRegion, testCfg.TableName)
+	uid1 := uuid.New().String()
+	now := time.Now()
+
+	t1 := mgr.NewTask(uid1, now)
+	require.NoError(t, t1.Save())
+
+	t2 := mgr.NewTask(uid1, now)
+	require.NoError(t, t2.Save())
+
+	tset, err := mgr.FetchTasks(uid1, now)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(tset))
+	assert.True(t, tset[0].TaskID == t1.TaskID || tset[1].TaskID == t1.TaskID)
+
+	assert.NoError(t, t1.Delete())
+	assert.NoError(t, t2.Delete())
 }
